056bffd73f2c01a0176f598d712b3e4136f86748
bibhutighimire <ghimirebibhuti@gmail.com>
2020-07-13 17:00:40 -0600

    commiting first design with form to test

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 // Citation
 //@Link https://www.youtube.com/watch?v=rsd4FNGTRBw
 
-var form=document.getElementById("form");
-var fname=document.getElementById("fname");
-var lname=document.getElementById("lname");
-var email=document.getElementById("email");
-var subject=document.getElementById("subject");
-var message=document.getElementById("message");
+var form = document.getElementById("form");
+var fname = document.getElementById("fname");
+var lname = document.getElementById("lname");
+var email = document.getElementById("email");
+var reenteremail = document.getElementById("reenteremail");
+var subject = document.getElementById("subject");
+var message = document.getElementById("message");
 
-form.addEventListener("submit", (c)=> {
-    
+form.addEventListener("submit", (c) => {
+
     c.preventDefault();
     CheckInputs();
 
 });
 
 function CheckInputs() {
-    const fnamevalue=fname.value.trim();
-    const lnamevalue=lname.value.trim();
-    const emailvalue=email.value.trim();
-    const subjectvalue=subject.value.trim();
-    const messagevalue=message.value.trim();
+    const fnamevalue = fname.value.trim();
+    const lnamevalue = lname.value.trim();
+    const emailvalue = email.value.trim();
+    const reenteremailvalue = reenteremail.value.trim();
+    const subjectvalue = subject.value.trim();
+    const messagevalue = message.value.trim();
 
-    if(fnamevalue==="") {
+    if (fnamevalue === "") {
 
         ErrorResult(fname, "First Name can not be blank");
 
@@ -31,7 +33,7 @@ function CheckInputs() {
         SuccessResult(fname);
     }
 
-    if(lnamevalue==="") {
+    if (lnamevalue === "") {
 
         ErrorResult(lname, "Last Name can not be blank");
 
@@ -40,38 +42,66 @@ function CheckInputs() {
         SuccessResult(lname);
     }
 
-    if(emailvalue==="") {
+    if (emailvalue === "") {
 
-        ErrorResult(email, "Last Name can not be blank");
+        ErrorResult(email, "Email Address can not be blank");
 
+    }
+    else if (!ValidateEmail(emailvalue)) {
+        ErrorResult(email, "Enter valid Email Address");
     }
     else {
         SuccessResult(email);
     }
+    if (reenteremailvalue === "") {
+        ErrorResult(reenteremail, "Email Address can not be blank");
+    }
+    else if (!ValidateEmail(reenteremailvalue)) {
+        ErrorResult(reenteremail, "Enter valid Email Address");
+    }
+    else if (emailvalue !== reenteremailvalue) {
+        ErrorResult(reenteremail, "Email Address entered do not match")
+    }
+    else {
+        SuccessResult(reenteremail);
+    }
+
+    if (subjectvalue === "") {
+
+        ErrorResult(subject, "Subject can not be blank");
+
+    }
+    else {
+        SuccessResult(subject);
+    }
+
+    if (messagevalue === "") {
+
+        ErrorResult(message, "Message can not be blank");
+
+    }
+    else {
+        SuccessResult(message);
+    }
 
 }
 //
-function ErrorResult(input,message) {
-    const formcontrol= input.parentElement; //form-control
-    const small=formcontrol.querySelector("small"); 
-    small.innerText=message;
-    formcontrol.className= "form-control error";
+function ErrorResult(input, message) {
+    const formcontrol = input.parentElement; //form-control
+    const small = formcontrol.querySelector("small");
+    small.innerText = message;
+    formcontrol.className = "form-control error";
 }
 function SuccessResult(input) {
-    const formcontrol= input.parentElement; //form-control
-    const small=formcontrol.querySelector("small");     
-    formcontrol.className= "form-control success";
+    const formcontrol = input.parentElement; //form-control
+    const small = formcontrol.querySelector("small");
+    formcontrol.className = "form-control success";
 }
 
 
 //@link https://www.w3resource.com/javascript/form/email-validation.php
 //This will check the validity of email address
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
+//https://codepen.io/FlorinPop17/pen/OJJKQeK
+function ValidateEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
