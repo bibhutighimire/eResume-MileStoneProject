b837ebdd330e1c5f969b3a0beb2ea9d364f0dd84
bibhutighimire <ghimirebibhuti@gmail.com>
2020-07-19 20:36:14 -0600

    final push and tomorrow is project demo

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Citation
 //@Link https://www.youtube.com/watch?v=rsd4FNGTRBw
-
+//instiating  ID's
 let form = document.getElementById("form");
 let fname = document.getElementById("fname");
 let lname = document.getElementById("lname");
@@ -9,61 +9,52 @@ let reenteremail = document.getElementById("reenteremail");
 let subject = document.getElementById("subject");
 let message = document.getElementById("message");
 
-
- const c=document.getElementById("sendEmailLink")
- if(c) {
-     c.addEventListener("click", (e) => {
-     e.preventDefault();
-     checkInputs();
- });
+//Contact Form JS Function. 
+const c = document.getElementById("sendEmailLink")
+if (c) {
+    c.addEventListener("click", (e) => {
+        e.preventDefault();
+        checkInputs();
+    });
 }
 //It will redirect to sending email step once check input function is completed
-function redirect()
-{
-    //window.location.href = "mailto:ghimirebibhuti@gmail.com";
+//Using mailto: to append message, subject and body element of comtact form to outlook
+function redirect() {
 
-//GETTING SUBJECT VALUE
+    //GETTING SUBJECT VALUE
     let subject = document.getElementById("subject").value;
-//GETTING message VALUE
-let message = document.getElementById("message").value;
-    document.location.href = "mailto:ghimirebibhuti@gmail.com?subject="
-    + subject +
-         "&body="  + message ;
+    //GETTING message VALUE
+    let message = document.getElementById("message").value;
+    document.location.href = "mailto:ghimirebibhuti@gmail.com?subject=" +
+        subject +
+        "&body=" + message;
+        //this will clear all text box of the form
+        form.reset();
 
-//this will clear text box
-
-    fname.value = "";
- lname.value = "";
- email.value = "";
- reenteremail.value = "";
- subject.value = "";
- message.value = "";
-
+    
 }
 //https://www.c-sharpcorner.com/blogs/block-specific-bad-words-using-javascript-jquery
-function swearWordsTracker() {  
-    let hasSwearWords=false;
-    let restrictedWords = new Array("kill", "fight", "slap","feldercarb", "frack", "skinjob", "vulgacarb");  
-    let txtInput = document.getElementById("message").value;  
-    let error = 0;  
-    for (let i = 0; i < restrictedWords.length; i++) {  
-        let val = restrictedWords[i];  
-        if ((txtInput.toLowerCase()).indexOf(val.toString()) > -1) {  
-            error = error + 1;  
+//Below method will use for loop to check every word in message.value and compare it with restricted words . if any of the word matches with restricted word, error will be error++ and error message is generated as well as it prevents form from submitting.
+function swearWordsTracker() {
+    let hasSwearWords = false;
+    let restrictedWords = new Array("kill", "fight", "slap", "feldercarb", "frack", "skinjob", "vulgacarb");
+    let txtInput = document.getElementById("message").value;
+    let error = 0;
+    for (let i = 0; i < restrictedWords.length; i++) {
+        let val = restrictedWords[i];
+        if ((txtInput.toLowerCase()).indexOf(val.toString()) > -1) {
+            error = error + 1;
+        }
+    }
 
-        }  
-    }  
-
-    if (error > 0) {  
+    if (error > 0) {
         ErrorResult(message, "You have used BAD word in the message. We will not tolerate this.");
-        hasSwearWords=true;        
-    }  
-    else {  
+        hasSwearWords = true;
+    } else {
         //it goes to redirect function only if forms are checked properly with bad word tracker and input checker
-        redirect();       
-    }  
-    
-}  
+        redirect();
+    }
+}
 //end of https://www.c-sharpcorner.com/blogs/block-specific-bad-words-using-javascript-jquery
 //checkinput function will check if there is any empty field in the contact form
 function checkInputs() {
@@ -77,80 +68,67 @@ function checkInputs() {
     //https://stackoverflow.com/questions/14452524/show-alert-if-any-bad-filtered-word-is-present-in-form-input
     //https://stackoverflow.com/questions/17384992/how-to-block-bad-words-upon-form-submit
 
-let hasErrors=false;
+    let hasErrors = false;
 
     if (fnamevalue === "") {
         ErrorResult(fname, "First Name can not be blank");
-        hasErrors=true;
-    }
-    else {
+        hasErrors = true;
+    } else {
         SuccessResult(fname);
     }
     if (lnamevalue === "") {
         ErrorResult(lname, "Last Name can not be blank");
-        hasErrors=true;
-    }
-    else {
+        hasErrors = true;
+    } else {
         SuccessResult(lname);
     }
     if (emailvalue === "") {
         ErrorResult(email, "Email Address can not be blank");
-        hasErrors=true;
-    }
-    else if (!ValidateEmail(emailvalue)) {
+        hasErrors = true;
+    } else if (!ValidateEmail(emailvalue)) {
         ErrorResult(email, "Enter valid Email Address");
-        hasErrors=true;
-    }
-    else {
+        hasErrors = true;
+    } else {
         SuccessResult(email);
     }
     if (reenteremailvalue === "") {
         ErrorResult(reenteremail, "Email Address can not be blank");
-        hasErrors=true;
-    }
-    else if (!ValidateEmail(reenteremailvalue)) {
+        hasErrors = true;
+    } else if (!ValidateEmail(reenteremailvalue)) {
         ErrorResult(reenteremail, "Enter valid Email Address");
-        hasErrors=true;
-    }
-    else if (emailvalue !== reenteremailvalue) {
+        hasErrors = true;
+    } else if (emailvalue !== reenteremailvalue) {
         ErrorResult(reenteremail, "Email Address entered do not match")
-        hasErrors=true;
-    }
-    else {
+        hasErrors = true;
+    } else {
         SuccessResult(reenteremail);
     }
 
     if (subjectvalue === "") {
 
         ErrorResult(subject, "Subject can not be blank");
-        hasErrors=true;
+        hasErrors = true;
 
-    }
-    else {
+    } else {
         SuccessResult(subject);
     }
 
     if (messagevalue === "") {
 
         ErrorResult(message, "Message can not be blank");
-        hasErrors=true;
-
-    }
-    else if (messagevalue === "feldercarb" || messagevalue === "frack" || messagevalue === "skinjob" || messagevalue === "vulgacarb"  || messagevalue === "bitch") {
-        ErrorResult(message, "Message should be in Proper Words");
-        hasErrors=true;
-    }
-
+        hasErrors = true;
+    } 
     else {
         SuccessResult(message);
     }
     //https://stackoverflow.com/questions/14452524/show-alert-if-any-bad-filtered-word-is-present-in-form-input
-
-    if(hasErrors===false){
+    //After checking for input, the if loop will goto swearWordsTracker() method.
+    if (hasErrors === false) {
         swearWordsTracker();
-     }
+    }
 }
 //function to deal with error result i.e. if there is empty input in contact form
+//Input: It will take the value from input box of contact form and provide message as output. 
 function ErrorResult(input, message) {
     const formcontrol = input.parentElement; //form-control
     const small = formcontrol.querySelector("small");
@@ -158,6 +136,7 @@ function ErrorResult(input, message) {
     formcontrol.className = "form-control error";
 }
 //function to deal with success result i.e. if there is no empty input in contact form
+//Input: It will take the value from input box of contact form
 function SuccessResult(input) {
     const formcontrol = input.parentElement; //form-control
     const small = formcontrol.querySelector("small");
@@ -168,6 +147,8 @@ function SuccessResult(input) {
 //@link https://www.w3resource.com/javascript/form/email-validation.php
 //This will check the validity of email address
 //https://codepen.io/FlorinPop17/pen/OJJKQeK
+//Expectation: when user enters email which is not of type email, the function below validates it.
+//Input: Email is passed from contact form email input field
 
 function ValidateEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
@@ -175,67 +156,74 @@ function ValidateEmail(email) {
 
 //back to top button
 //Get the button
+//Expectation: To get Back To Top Bottom on bottom right of screen when user scrolls 20px down
 const mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+    ScrollFunction()
+};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+function ScrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function TopFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 //end of back to top button https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
 
 //filter JS function start
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+//This is a button filter used in index.html starting from Line number 206. It expects us to eith show all, wrapper design, graphics deign, media design, web design and others. 
+FilterSelection("all")
 
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+function FilterSelection(c) {
+    let x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        W3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) W3AddClass(x[i], "show");
     }
-  }
-  element.className = arr1.join(" ");
+}
+
+function W3AddClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function W3RemoveClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
 }
 
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+let btnContainer = document.getElementById("myBtnContainer");
+let btns = document.getElementsByClassName(".btn");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
 //filter js function end
